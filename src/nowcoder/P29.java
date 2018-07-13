3c859875db13119cbe87cfc5b2ba7e4e73d9076b
mwindson <mwindsoncn@gmail.com>
2018-07-13 23:04:27 +0800

    update

---
+++ src/nowcoder/P29.java
@@ -0,0 +1,17 @@
+
+public class P29 {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if (array == null || array.length == 0) return 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                sum = sum >= 0 ? sum + array[i] : array[i];
+            } else {
+                sum = sum > 0 ? sum + array[i] : array[i];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
